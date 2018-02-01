0f7693e74febd93c3479db8faf25039964c52bf2
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-02-01 15:33:42 -0600

    Create Lab1Week2.c

---
+++ Lab1Week2.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : Lab1Week2.c
+ Author      : Tsiania Hughes
+ Version     :
+ Copyright   : Your copyright notice: it's mine, Taz
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <asm/io.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LED1  8		// wiringPi number corresponding to GPIO2. RED
+					//Check Figures 2 and 3 in the Lab9 guide.
+#define LED2  9		//YELLOW
+#define LED3  7		//GREEN
+#define LED4  21	//BLUE
+
+
+//sudo insmod Lab1Week2.ko
+//sudo rmmod Lab1Week2
+
+
+
+
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
