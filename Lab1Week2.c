1a89aff9e1af4f8b2c094baf539b1f6be3e3508c
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-02-05 16:07:46 -0600

    Update Lab1Week2.c

--- Lab1Week2.c
+++ Lab1Week2.c
@@ -21,34 +21,37 @@
 #define __KERNEL__
 #endif
 
-
 MODULE_LICENSE("GPL");
-
 
 int init_module(void)
 {
-	// your code here
-	printk("Loading...");
-	//pointer to hold
-	unsigned long *SelectPin= (unsigned long)*ioremap((0x3F200000),4096);
-	iowrite32((*SelectPin | 0x00049240), SelectPin);
-	SelectPin+=7;//change to GPIO Select
-	iowrite((0x0000003C),SelectPin);
-	printk("Loading Done!");
+//pointer to hold
+unsigned long *SelectPin= (unsigned long*)ioremap((0x3F200000),4096);
+//could make this a global variable to set to specific registers to different names
+// your code here
+printk("Loading...");
 
-	return 0;
+iowrite32((*SelectPin | 0x00049240), SelectPin);
+SelectPin+=7;//to move to register GPIO Set
+iowrite32((0x0000003C),SelectPin);
+printk("Loading Done!");
+
+return 0;
 }
 
 void cleanup_module(void)
 {
-	// your code here
-	printk("Stopping...");
-	unsigned long *SelectPin= (unsigned long)*ioremap((0x3F200000),4096);
-	iowrite32((*SelectPin | 0x00049240), SelectPin);
-	SelectPin+=10;//GPIO Clear
-	iowrite((0x0000003C),SelectPin);
-	printk("Stopped");
+unsigned long *SelectPin= (unsigned long*)ioremap((0x3F200000),4096);
+
+// your code here
+printk("Stopping...");
+
+iowrite32((*SelectPin | 0x00049240), SelectPin);
+SelectPin+=10;//to move to GPIO clear
+iowrite32((0x0000003C),SelectPin);
+printk("Stopped");
 }
+
 
 /*MODULE_init(init_module);
 MODULE_exit(cleanup_module);*/ 
