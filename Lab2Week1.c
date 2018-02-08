c2bbb865f89278e7203b5342d49920a7a2819689
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-02-08 15:41:09 -0600

    Update Lab2Week1.c

--- Lab2Week1.c
+++ Lab2Week1.c
@@ -1,1 +1,30 @@
-//Stuff
+/*
+ ============================================================================
+ Name        : Lab2Week1.c
+ Author      : Tsiania Hughes
+ Version     :
+ Copyright   : Your copyright notice: it's mine, Taz
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct sthreads{
+	int Neo_matrix[][];
+	int count[];
+};
+
+
+int main(argc, *argv[]) {
+
+	if(argc!=3)
+	{
+		printf("\nWrong Amount of Arguments\n");
+		return 0;
+	}
+
+	return EXIT_SUCCESS;
+}
