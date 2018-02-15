3664e911557a0afa37bba995589a90782fa27a9c
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-02-14 22:15:02 -0600

    Update Lab2Week1.c

--- Lab2Week1.c
+++ Lab2Week1.c
@@ -11,20 +11,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
-typedef struct sthreads{
-	int Neo_matrix[][];
+//struct
+typedef struct{
+	int neo_matrix[][];
 	int count[];
-};
+}sthreads;
 
+void *OneThreadToRuleThemAll(int ptr[][]){
+
+
+}
+void *OneThreadR(void *ptr){
+
+}
+void *OneThreadC(void *ptr){
+
+}
+
+void *OneThreadE(void *ptr){
+
+}
 
 int main(argc, *argv[]) {
+	//variables
+	int targetNum;
+	int rows;
+	int cols;
 
+	sthreads hold=hold.neo_matrix[rows][cols];
+
+	// a.out,  text file and target number
 	if(argc!=3)
 	{
 		printf("\nWrong Amount of Arguments\n");
 		return 0;
 	}
+	//collecting variables from file
+	FILE *fptr=fopen(argv[1],"r");
+	if(fptr==NULL)
+		{
+			printf("\nFile not Found or Incorrect placing");
+			printf("\n Lab2.out matrix.txt #");
+			return 0;
+		}
+	fscanf(fptr, "%d %d", rows, cols);
 
-	return EXIT_SUCCESS;
+
+
+	sthreads hold=hold.neo_matrix[rows][cols];
+	//getting target num
+	if(isdigit(atoi(argv[2]))!=0) //small check
+		targetNum=atoi(argv[2]);
+	//getting values out of text file into hold
+	while(!(feof(fptr))){
+		for(int x=0;x<rows; x++)
+			for(int y=0;y<cols;y++)
+			{
+				fscanf(fptr,"%d ",hold.neo_matrix[x][y]);
+			}
+
+	}
+	fclose(fptr);
+
+
+
+	return 0;
 }
