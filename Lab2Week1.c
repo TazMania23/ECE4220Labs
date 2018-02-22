fbf754f43bdbf9f9dcf4d6caf00de3fbca1c9ede
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-02-22 13:30:29 -0600

    Update Lab2Week1.c

--- Lab2Week1.c
+++ Lab2Week1.c
@@ -22,7 +22,15 @@ typedef struct {
 	int r;
 	int c;
 	int clue;
+;
 }sthreads;
+
+//made another struct, wasnt sure how to differeniate which thread was which
+typedef struct {
+	//thread id
+	int tid[100];
+	sthreads *storage;
+}otherThreads;
 
 void *OneThreadToRuleThemAll(void *ptr) {
 	//getting info from ptr 
@@ -47,22 +55,58 @@ void *OneThreadToRuleThemAll(void *ptr) {
 void *OneThreadR(void *ptr) {
 
 	//getting info from ptr 
-	sthreads *info = (sthreads*) ptr;
-	int num = (*info).clue;
-	int rows = (*info).r;
-	int cols = (*info).c;
+	otherThreads *Tinfo = (otherThreads*) ptr;
+	sthreads *Ginfo= Tinfo->storage;
 	
+	int id= Tinfo->tid;
+	int num = (*Ginfo).clue;
+	int rows = (*Ginfo).r;
 	
+	int x;
+	for(x=0;x<rows;x++)
+	{
+		if((*Ginfo).neo_matrix[id][x]==num)
+			++(*Ginfo).count[id];
+	}
 	
 	pthread_exit(0);
 }
 void *OneThreadC(void *ptr) {
+	
+	//getting info from ptr 
+	otherThreads *Tinfo = (otherThreads*) ptr;
+	sthreads *Ginfo= Tinfo->storage;
+	
+	int id= Tinfo->tid;
+	int num = (*Ginfo).clue;
+	int cols = (*Ginfo).c;
+	
+	int x;
+	for(x=0;x<cols;x++)
+	{
+		if((*Ginfo).neo_matrix[x][id]==num)
+			++(*Ginfo).count[id];
+	}
 	
 	pthread_exit(0);
 }
 
 void *OneThreadE(void *ptr) {
 
+	//getting info from ptr 
+	otherThreads *Tinfo = (otherThreads*) ptr;
+	sthreads *Ginfo= Tinfo->storage;
+	
+	int id= Tinfo->tid;
+	int num = (*Ginfo).clue;
+	int rows = (*Ginfo).r;
+	int cols = (*Ginfo).c;
+	int x;
+	//no for loop needed since there will be a thread for each element
+		if((*Ginfo).neo_matrix[rows][cols]==num)
+			++(*Ginfo).count[id];
+	
+	
 	pthread_exit(0);
 }
 
@@ -71,7 +115,7 @@ int main(int argc, char *argv[]) {
 	int targetNum;
 	int rows;
 	int cols;
-
+	int x,y;
 	sthreads hold;
 
 	// a.out,  text file and target number
@@ -93,6 +137,7 @@ int main(int argc, char *argv[]) {
 
 	hold.r = rows;
 	hold.c = cols;
+	hold.tid={0};
 	printf("\nRows and Coloums found");
 	//getting target num
 	if (isdigit(atoi(argv[2])) != 0) //small check
@@ -101,8 +146,8 @@ int main(int argc, char *argv[]) {
 	hold.clue = targetNum;
 	//getting values out of text file into hold
 	while (!(feof(fptr))) {
-		for (int x = 0; x<rows; x++)
-			for (int y = 0; y<cols; y++)
+		for (x = 0; x<rows; x++)
+			for (y = 0; y<cols; y++)
 			{
 				fscanf(fptr, "%d ", &hold.neo_matrix[x][y]);
 			}
@@ -124,7 +169,7 @@ int main(int argc, char *argv[]) {
 	clock_gettime(CLOCK_MONOTONIC, &endingT);
 	
 	number=1000000000 *(endingT.tv_sec - startingT.tv_sec)+ endingT.tv_nsec - startingT.tv_nsec;
-	printf("\nOne Thread to search whole matrix took %u ns", number);
+	printf("\nOne Thread to search whole matrix took %u ns\n\n", number);
 	
 	//since hold is getting passed into everything count must reset after ever thread finishes
 	int reset=0;
@@ -133,21 +178,104 @@ int main(int argc, char *argv[]) {
 	
 	//1 thread for searching each row
 	
+	pthread_t T2[rows];
+	otherThreads T2_info[rows]
+	clock_gettime(CLOCK_MONOTONIC, &startingT);
+	for(x=0;x<rows;x++)
+	{
+		//had issues here implementing
+		T2_info[x].tid=x;
+		T2_info[x].storage= &hold;
+		pthread_create(&T2[x], NULL, (void*)&OneThreadR, (void*)&T2_info[x]);
+	}
+	for(x=0;x<rows;x++)
+		pthread_join(T2[x], NULL);
+
+	clock_gettime(CLOCK_MONOTONIC, &endingT);
+	number=1000000000 *(endingT.tv_sec - startingT.tv_sec)+ endingT.tv_nsec - startingT.tv_nsec;
+	printf("\nOne Thread per each row in matrix took %u ns", number);
 	
-	pthread_t T2;
-	pthread_create(&T2, NULL, (void*)&OneThreadR, (void*)&hold);
-	pthread_join(T2, NULL);
+	//Sicne theres multiple threads per row to find the total loop all threads and add up
+	//their count values
+	int countByrow=0;
+	for(x=0;x<250;x++)
+		countByrow+=hold.count[x];
 	
-	
+	printf("\nSearching: %d \tCounts: %d\n\n",targetNum, countByrow); 
+	//counter reset
+	for(reset=0;reset<250;reset++)
+		hold.count[reset]=0;
 	
 	//1 thread for searching each column
-	pthread_t T3;
-	pthread_create(&T3, NULL, (void*)&OneThreadC, (void*)&hold);
-	pthread_join(T3, NULL);
-	//1 thread for each element
+	//basically copy and paste from the other one just change row to col
+	//please work right
+	pthread_t T3[cols];
+	otherThreads T3_info[cols];
+	
+	clock_gettime(CLOCK_MONOTONIC, &startingT);
+	
+	for(x=0;x<cols;x++)
+	{
+		//had issues here implementing
+		T3_info[x].tid=x;
+		T3_info[x].storage= &hold;
+		pthread_create(&T3[x], NULL, (void*)&OneThreadC, (void*)&T3_info[x]);
+	}
+	for(x=0;x<cols;x++)
+		pthread_join(T3[x], NULL);
 
-	pthread_t T4;
-	pthread_create(&T4, NULL, (void*)&OneThreadE, (void*)&hold);
-	pthread_join(T4, NULL);
+	clock_gettime(CLOCK_MONOTONIC, &endingT);
+	number=1000000000 *(endingT.tv_sec - startingT.tv_sec)+ endingT.tv_nsec - startingT.tv_nsec;
+	printf("\nOne Thread per each column in matrix took %u ns", number);
+	
+	//Sicne theres multiple threads per row to find the total loop all threads and add up
+	//their count values
+	int countBycol=0;
+	for(x=0;x<250;x++)
+		countBycol+=hold.count[x];
+	
+	printf("\nSearching: %d \tCounts: %d\n\n",targetNum, countBycol); 
+	//counter reset
+	for(reset=0;reset<250;reset++)
+		hold.count[reset]=0;
+	
+	//1 thread for each element
+	int totalElements=rows*cols;
+	
+	pthread_t T4[totalElements];
+	otherThreads T4_info[totalElements];
+	clock_gettime(CLOCK_MONOTONIC, &startingT);
+	
+	
+	for(x=0;x<totalElements;x++)
+	{
+		//had issues here implementing
+		T4_info[x].tid=x;
+		T4_info[x].storage= &hold;
+		pthread_create(&T4[x], NULL, (void*)&OneThreadE, (void*)&T4_info[x]);
+	}
+	
+	for(x=0;x<totalElements;x++)
+		pthread_join(T4[x], NULL);
+
+	clock_gettime(CLOCK_MONOTONIC, &endingT);
+	number=1000000000 *(endingT.tv_sec - startingT.tv_sec)+ endingT.tv_nsec - startingT.tv_nsec;
+	printf("\nOne Thread per each row in matrix took %u ns", number);
+	
+	//Sicne theres multiple threads per row to find the total loop all threads and add up
+	//their count values
+	int countByE=0;
+	for(x=0;x<250;x++)
+		countByE+=hold.count[x];
+	
+	printf("\nSearching: %d \tCounts: %d\n\n",targetNum, countByE); 
+	//counter reset
+	for(reset=0;reset<250;reset++)
+		hold.count[reset]=0;
+	
+	
+	//Done hopefully -_-
+	
+	
 	return 0;
 }
