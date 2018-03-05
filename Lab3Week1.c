393c83797b080ea0fcf23091b7ae859f40955b50
Tsiania Hughes <tdh275@mail.missouri.edu>
2018-03-04 18:09:40 -0600

    Should Be Done, need to test

--- Lab3Week1.c
+++ Lab3Week1.c
@@ -13,38 +13,79 @@
 #include <unistd.h>		// standard symbolic constants and types (e.g. NULL); usleep
 #include <wiringPi.h>	// needed for the wiringPi functions
 #include <pthread.h>
+#include <sched.h>
 
 #include "../Release/src/ece4220lab3.h"
 
-#define LED1  8		//RED- Pedistrians
-#define LED2  9		//YELLOW Light 2
-#define LED3  7		//GREEN Light 1
+#define LEDR  8		//RED- Pedistrians
+#define LEDY  9		//YELLOW Light 2
+#define LEDG  7		//GREEN Light 1
+#define LEDB 21		//Blue light only to define and shut off
+#define PBUTTON  27	//far Left
+#define PBUTTON2 28 //far Right
 
-#define PBUTTON  27
-
-int check_button(void)
+//the single thread for the Null sequence. Polled only needs one
+void Mainthread(void* ptr)
 {
+	printf("Polled Scheduling Commencing\n");
+	//setting the thread priority higher
+	struct sched_param param;
+	param.sched_priority=51;
+	sched_setscheduler(0,SCHED_FIFO, &param);
 
-	return 0;
+	//setting lights to off
+	digitalWrite(LEDR,LOW);
+	digitalWrite(LEDY,LOW);
+	digitalWrite(LEDG,LOW);
+	digitalWrite(LEDB,LOW);
+
+	int *hold= (int*) ptr;
+
+	printf("Starting Loop of Lights, Push rightmost button to end\n");
+
+	while(digitalRead(PBUTTON2)==FALSE)
+	{
+		//Green
+		digitalWrite(LEDG,HIGH);
+		sleep(1);
+		digitalWrite(LEDG,LOW);
+
+		//Yellow
+		digitalWrite(LEDY,HIGH);
+		sleep(1);
+		digitalWrite(LEDY,LOW);
+
+		if(check_button()==1)
+		{
+			digitalWrite(LEDR, HIGH);
+			clear_button();
+
+			sleep(2);
+			digitalWrite(LEDR,LOW);
+		}
+	}
+
 }
-void clear_button(void)
-{
-
-}
-
 int main(void) {
-
+//setting up GPIO
 	wiringPiSetup();
 
-	pinMode(LED1, OUTPUT);
-	pinMode(LED2, OUTPUT);
-	pinMode(LED3, OUTPUT);
+	pinMode(LEDR, OUTPUT);
+	pinMode(LEDY, OUTPUT);
+	pinMode(LEDG, OUTPUT);
+	pinMode(LEDB, OUTPUT);
 	pinMode(PBUTTON, INPUT);
-
+	pinMode(PBUTTON2, INPUT);
+//so button will be True until pushed
 	pullUpDnControl(PBUTTON, PUD_DOWN);
+	pullUpDnControl(PBUTTON2, PUD_DOWN);
 
-	while(1)
-	{
+	printf("Creating Threads\n");
+	pthread_t Polled;
+	int num=0;
+	//creating the lone thread
+	pthread_create(&Polled, NULL, (void*)Mainthread, (void*) &num);
+	pthread_join(Polled, NULL);
 
-	}
+	return 0;
 }
